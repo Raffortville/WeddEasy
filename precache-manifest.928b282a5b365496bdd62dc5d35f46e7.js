8bc4eade594f70bdef66e010b42d6d8f2766bd43
Raffortville <raffihaycan@gmail.com>
2020-11-05 13:44:42 +0400

    Updates

---
+++ precache-manifest.928b282a5b365496bdd62dc5d35f46e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdd5a38beeb948fe6f3af387783291fd",
+    "url": "/Raffortville/WeddEasy.git/index.html"
+  },
+  {
+    "revision": "698a260d1255986bdc36",
+    "url": "/Raffortville/WeddEasy.git/static/css/main.46810e57.chunk.css"
+  },
+  {
+    "revision": "3ba775c17eac61afdb47",
+    "url": "/Raffortville/WeddEasy.git/static/js/2.016ea448.chunk.js"
+  },
+  {
+    "revision": "ce57b80e6a20985ad722d14a211a19e1",
+    "url": "/Raffortville/WeddEasy.git/static/js/2.016ea448.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "698a260d1255986bdc36",
+    "url": "/Raffortville/WeddEasy.git/static/js/main.23e7932a.chunk.js"
+  },
+  {
+    "revision": "6be1825ef97e7a9fc39a",
+    "url": "/Raffortville/WeddEasy.git/static/js/runtime-main.a3e60c41.js"
+  }
+]);
